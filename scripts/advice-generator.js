fe9e2ea7eefab7c53b591e0050bf2b5febb7d15c
Robert Hardy <rhardy1@dcskk12.org>
2023-08-23 15:39:26 -0600

    Added advice generator

---
+++ scripts/advice-generator.js
@@ -0,0 +1,25 @@
+let advisors = [
+	"A wise man once told me",
+	"Mother always says",
+	"My Uncle Rupert once said",
+];
+
+let howOften = ["never", "always", "as often as you can", "never ever"];
+
+let verbs = ["run with", "swim in", "eat", "climb", "dance with"];
+
+let adjectives = ["spiky", "furry", "funny", "spicy", "odd", "loud", "scary"];
+
+let nouns = [
+	"elephants",
+	"humans",
+	"chainsaws",
+	"nuclear bombs",
+	"computers",
+	"commuters",
+	"video games",
+];
+
+let rand = Math.floor(Math.random() * 100);
+
+console.log(rand);
---
+++ scripts/basics-1.js
@@ -0,0 +1,33 @@
+// any I that I type after the slashes will be a comment
+
+/* 
+multiline comment
+this will
+span
+many lines
+*/
+
+//console.log("Hello world!");
+
+// keywords for declaring variables
+
+// strings
+let favoriteSong = "Never gonna give you up";
+
+// numbers
+let age = 52;
+let myMoney = -10000.45;
+
+// boolean
+let isGameOver = false;
+
+// arrays
+// a variable that holds a "list" of values
+
+let fruits = ["apple", "grapes", "banana"];
+
+console.log(fruits[2]);
+
+myMoney = "bankrupt";
+
+//console.log(favoriteSong.length);
