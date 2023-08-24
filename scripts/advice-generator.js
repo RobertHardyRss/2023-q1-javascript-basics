31f44b1cdf9680e338a9c9c3a9d399520bf17dcd
Robert Hardy <rhardy1@dcskk12.org>
2023-08-24 16:13:44 -0600

    Update random advice generator

--- scripts/advice-generator.js
+++ scripts/advice-generator.js
@@ -20,6 +20,41 @@ let nouns = [
 	"video games",
 ];
 
-let rand = Math.floor(Math.random() * 100);
+let getRandomIndex = function (a) {
+	let rand = Math.random() * a.length;
+	return Math.floor(rand);
+};
 
-console.log(rand);
+let getRandomWord = function (a) {
+	return a[getRandomIndex(a)];
+};
+
+let generateAdvice = function (a, h, v, adj, n) {
+	console.log(
+		getRandomWord(a),
+		getRandomWord(h),
+		getRandomWord(v),
+		getRandomWord(adj),
+		getRandomWord(n)
+	);
+};
+
+generateAdvice(advisors, howOften, verbs, adjectives, nouns);
+
+let rand = getRandomIndex(advisors);
+//let rand = Math.floor(Math.random() * advisors.length);
+let randAdvisor = advisors[rand];
+
+rand = Math.floor(Math.random() * howOften.length);
+let randHowOften = howOften[rand];
+
+rand = Math.floor(Math.random() * adjectives.length);
+let randAdjectives = adjectives[rand];
+
+rand = Math.floor(Math.random() * verbs.length);
+let randVerb = verbs[rand];
+
+rand = Math.floor(Math.random() * nouns.length);
+let randNouns = nouns[rand];
+
+console.log(randAdvisor, randHowOften, randVerb, randAdjectives, randNouns);
